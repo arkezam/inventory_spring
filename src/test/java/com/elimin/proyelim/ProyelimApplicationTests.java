9702ce48867f72af428b9a8bb8f112936a4cc40b
arkezam <arkezam@gmail.com>
2023-03-26 19:00:59 -0500

    proyect starter

---
+++ src/test/java/com/elimin/proyelim/ProyelimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elimin.proyelim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyelimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
